623a71315bd3cd17d66bfa74d190ca54400d157c
Markus Wiland <markus.wiland@outlook.com>
2024-07-12 11:40:57 +0200

    added uppdrag page

---
+++ app/uppdrag/[id]/page.tsx
@@ -0,0 +1,72 @@
+import { ButtonComponent } from "@/app/_components/button";
+import { Container } from "@/app/_components/container";
+import { HeroTitle } from "@/app/_components/hero";
+import {
+  Card,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import Image from "next/image";
+import React from "react";
+interface Uppdrag {
+  id: string;
+  title: string;
+  description: string;
+  image: string;
+}
+export default function UppdragPage({ params }: { params: { id: string } }) {
+  return (
+    <Container className="pt-[6.4rem]">
+      <ButtonComponent href="/uppdrag" variant="primary" size="large" className="mb-6">
+        Gå tillbaka
+      </ButtonComponent>
+      <Card className="p-6">
+        <CardHeader className="p-0">
+          <Image
+            src="https://via.placeholder.com/1000"
+            width={1000}
+            height={200}
+            className="w-full h-[20rem] rounded-lg"
+            objectFit="cover"
+            alt="placeholder"
+          />
+          <CardTitle>
+            <HeroTitle>Front End Utvecklare</HeroTitle>
+          </CardTitle>
+        </CardHeader>
+        <CardDescription className="flex gap-4">
+          <div className="md:basis-2/3">
+            VI söker just nu en Front End utvecklare som kan hjälpa oss med att
+            bygga en ny hemsida. Vi söker dig som har erfarenhet av att arbeta
+            med React och Typescript.
+          </div>
+          <div className="flex gap-4 md:basis-1/3">
+            <div className="space-y-4">
+              <h2 className="font-bold uppercase">Erfarenheter</h2>
+              <ul>
+                <li>3 års erfarenhet</li>
+                <li>React</li>
+                <li>Typescript</li>
+                <li>HTML</li>
+                <li>CSS</li>
+              </ul>
+            </div>
+            <div className="space-y-4">
+              <h2 className="font-bold uppercase">Extra erfanrehter</h2>
+              <ul className="">
+                <li>3 års erfarenhet</li>
+                <li>React</li>
+                <li>Typescript</li>
+                <li>HTML</li>
+                <li>CSS</li>
+              </ul>
+            </div>
+          </div>
+        </CardDescription>
+        <CardFooter>Footer</CardFooter>
+      </Card>
+    </Container>
+  );
+}
--- app/uppdrag/page.tsx
+++ app/uppdrag/page.tsx
@@ -18,28 +18,34 @@ import {
 } from "@/components/ui/pagination";
 import { HeroSubtitle, HeroTitle } from "../_components/hero";
 import { ButtonComponent } from "../_components/button";
+import Link from "next/link";
 const uppdrag = [
   {
+    id: 1,
     title: "Uppdrag 1",
     description:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sit amet purus sed justo tincidunt cursus. Sed nec eros vel nulla lacinia lacinia",
   },
   {
+    id: 2,
     title: "Uppdrag 2",
     description:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sit amet purus sed justo tincidunt cursus. Sed nec eros vel nulla lacinia lacinia",
   },
   {
+    id: 3,
     title: "Uppdrag 3",
     description:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sit amet purus sed justo tincidunt cursus. Sed nec eros vel nulla lacinia lacinia",
   },
   {
+    id: 4,
     title: "Uppdrag 4",
     description:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sit amet purus sed justo tincidunt cursus. Sed nec eros vel nulla lacinia lacinia",
   },
   {
+    id: 5,
     title: "Uppdrag 5",
     description:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sit amet purus sed justo tincidunt cursus. Sed nec eros vel nulla lacinia lacinia",
@@ -52,34 +58,37 @@ export default function Uppdrag() {
       <HeroSubtitle>Hitta ditt uppdrag</HeroSubtitle>
       <div className="divide-y divide-whiteopaque bg-[#201726] border border-whiteopaque rounded-2xl">
         {uppdrag.map((item, index) => (
-          <div
-            key={index}
-            className={`p-3 md:p-7 group flex hover:bg-whiteopaque transition-colors duration-300 cursor-pointer`}
-          >
-            {/* Första kortet */}
-            <div className="">
-              <img
-                src="https://via.placeholder.com/300"
-                alt="placeholder"
-                className="w-full h-full rounded-lg"
-              />
+          <Link href={`uppdrag/${item.id}`} key={index}>
+            <div
+              key={index}
+              className={`p-3 md:p-7 group flex hover:bg-whiteopaque transition-colors duration-300 cursor-pointer`}
+            >
+              <div className="">
+                <img
+                  src="https://via.placeholder.com/300"
+                  alt="placeholder"
+                  className="w-full h-full rounded-lg"
+                />
+              </div>
+              <div className="space-y-6 px-6 flex flex-col">
+                <CardHeader className="p-0">
+                  <HeroTitle className="md:text-xl my-0">
+                    {item.title}
+                  </HeroTitle>
+                </CardHeader>
+                <CardDescription>
+                  <HeroSubtitle className="md:text-sm">
+                    {item.description}
+                  </HeroSubtitle>
+                </CardDescription>
+                <CardFooter className="p-0">
+                  <ButtonComponent href={"/"} variant="primary" size="large">
+                    Läs mer{" "}
+                  </ButtonComponent>
+                </CardFooter>
+              </div>
             </div>
-            <div className="space-y-6 px-6 flex flex-col">
-              <CardHeader className="p-0">
-                <HeroTitle className="md:text-xl my-0">{item.title}</HeroTitle>
-              </CardHeader>
-              <CardDescription>
-                <HeroSubtitle className="md:text-sm">
-                  {item.description}
-                </HeroSubtitle>
-              </CardDescription>
-              <CardFooter className="p-0">
-                <ButtonComponent href={"/"} variant="primary" size="large">
-                  Läs mer{" "}
-                </ButtonComponent>
-              </CardFooter>
-            </div>
-          </div>
+          </Link>
         ))}
       </div>
       <Pagination>
